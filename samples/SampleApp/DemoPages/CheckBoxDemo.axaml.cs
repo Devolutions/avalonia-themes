ecc1810a8488abf86068cfae51574051dc322e68
Anna Malchow-Perryman <amalchowperryman@gmail.com>
2024-12-05 21:58:43 -0500

    Add CheckBox styling & cleaner Button borders (#6)
    * Tidy up
    
    * Add CheckBox styling
    
    * Adjust button borders to look more accurate
    
    * Add view on gray bg to Sampler

---
+++ samples/SampleApp/DemoPages/CheckBoxDemo.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SampleApp.DemoPages;
+
+public partial class CheckBoxDemo : UserControl
+{
+  public CheckBoxDemo()
+  {
+    InitializeComponent();
+  }
+}
