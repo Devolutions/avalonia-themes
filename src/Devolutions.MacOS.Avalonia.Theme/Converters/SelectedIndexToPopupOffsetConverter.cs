4e61098ac341ba468242bbdae9e96654b72d997d
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-01-08 16:59:16 -0500

    Add top & bottom padding to ComboBox popup (& improve var naming)

--- src/Devolutions.MacOS.Avalonia.Theme/Converters/SelectedIndexToPopupOffsetConverter.cs
+++ src/Devolutions.MacOS.Avalonia.Theme/Converters/SelectedIndexToPopupOffsetConverter.cs
@@ -7,12 +7,13 @@ public class SelectedIndexToPopupOffsetConverter : IMultiValueConverter
 {
   public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
   {
-    if (values is not [int index, int rowHeight, int initialBottomMargin, double maxDropDownHeight, int popupTrimHeight]
+    if (values is not
+          [int index, int rowHeight, int initialFirstItemDistance, double maxDropDownHeight, int popupTrimHeight]
         || index < 0) return 0d;
 
     var effectivePopupHeight = maxDropDownHeight - popupTrimHeight;
-    double baseOffset = (index + 1) * -rowHeight - initialBottomMargin;
-    return -effectivePopupHeight < baseOffset ? baseOffset : -effectivePopupHeight;
+    double offset = (index + 1) * -rowHeight - initialFirstItemDistance;
+    return -effectivePopupHeight < offset ? offset : -effectivePopupHeight;
   }
 
   public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
