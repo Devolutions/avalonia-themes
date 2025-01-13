25db35028432109e8ca13444ee6be05e39635528
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-01-13 12:09:21 -0500

    Add additional DataGrid styling

--- samples/SampleApp/ViewModels/DataGridViewModel.cs
+++ samples/SampleApp/ViewModels/DataGridViewModel.cs
@@ -13,7 +13,11 @@ public class DataGridViewModel : ObservableObject
     {
       new("computer", "PDQ-VM", "PDQ-VM$", "6/26/2023"),
       new("user", "AD TEST Guy2", "ADGuysam", "5/2/2023"),
-      new("user", "krbtgt", "krbtgt", "11/10/2021")
+      new("user", "krbtgt", "krbtgt", "11/10/2021"),
+      new("computer", "WIN11-VM-BT", "WIN11-VM-BT$", "11/12/2021"),
+      new("computer", "RDS6", "RDS6$", "30/03/2021"),
+      new("computer", "MSSQL1", "MSSQL1$", "14/10/2024"),
+      new("computer", "ITMANGER-DRIVE", "ITMANGER-DRIVE$", "13/10/2021")
     };
     Items = new ObservableCollection<DataGridItem>(items);
   }
