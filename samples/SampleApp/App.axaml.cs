f7582020a59bde1b2784e33133ecd5d750acdc12
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-01-23 01:17:16 -0500

    [WIP] Allow style change for disabled icons

--- samples/SampleApp/App.axaml.cs
+++ samples/SampleApp/App.axaml.cs
@@ -1,6 +1,8 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Svg.Skia;
 
 namespace SampleApp;
 
@@ -9,6 +11,9 @@ public class App : Application
   public override void Initialize()
   {
     AvaloniaXamlLoader.Load(this);
+
+    GC.KeepAlive(typeof(Avalonia.Svg.Skia.Svg).Assembly);
+    GC.KeepAlive(typeof(SvgImageExtension).Assembly);
   }
 
   public override void OnFrameworkInitializationCompleted()
