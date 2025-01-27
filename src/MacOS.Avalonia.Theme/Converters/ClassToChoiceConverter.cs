0a175bddecc1c647181f95b691bc06251c3c6284
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-01-27 14:04:40 -0500

    Ensure flexibility for external styling

--- src/MacOS.Avalonia.Theme/Converters/ClassToChoiceConverter.cs
+++ src/MacOS.Avalonia.Theme/Converters/ClassToChoiceConverter.cs
@@ -9,9 +9,11 @@ public class ClassToChoiceConverter : IMultiValueConverter
 {
   public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
   {
+    var secondOption = values.Count >= 3 ? values[2] : AvaloniaProperty.UnsetValue;
+
     var selectedChoice = values[0] is Classes classes && parameter is string className && classes.Contains(className)
       ? values[1]
-      : values[2];
+      : secondOption;
     return selectedChoice ?? AvaloniaProperty.UnsetValue;
   }
 
