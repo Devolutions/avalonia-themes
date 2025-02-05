73242be6c6ecac07aae8eab57202bac5b6e84083
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-02-04 19:10:09 -0500

    Remove redundant `Console.Write`s

--- src/MacOS.Avalonia.Theme/Converters/ThicknessToSelectiveThicknessConverter.cs
+++ src/MacOS.Avalonia.Theme/Converters/ThicknessToSelectiveThicknessConverter.cs
@@ -8,13 +8,9 @@ public class ThicknessToSelectiveThicknessConverter : IValueConverter
 {
   public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
   {
-    Console.WriteLine($"value: {value}");
-    Console.WriteLine($"parameter: {parameter}");
     if (value is not Thickness thickness || parameter is not Thickness thicknessFactors)
       return AvaloniaProperty.UnsetValue;
 
-    Console.WriteLine($"thickness: {thickness}");
-    Console.WriteLine($"thicknessFactors: {thicknessFactors}");
     return new Thickness(
       thickness.Left * thicknessFactors.Left,
       thickness.Top * thicknessFactors.Top,
