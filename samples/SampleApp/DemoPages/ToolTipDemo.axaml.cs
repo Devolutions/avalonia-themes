c26d13cfb952b57b90b412146dd6d5e1714b59dc
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-02-18 23:17:24 -0500

    Add Tooltip styling (#58)

---
+++ samples/SampleApp/DemoPages/ToolTipDemo.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SampleApp.DemoPages;
+
+public partial class ToolTipDemo : UserControl
+{
+  public ToolTipDemo()
+  {
+    InitializeComponent();
+  }
+}
