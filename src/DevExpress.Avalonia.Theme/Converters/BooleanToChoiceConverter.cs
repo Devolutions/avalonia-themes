3c32f892eac59fc731e3ae7f5fc40f0502ea123c
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-03-12 21:34:44 +0100

    [DevExpress] Add ComboBox styling (#92)

---
+++ src/DevExpress.Avalonia.Theme/Converters/BooleanToChoiceConverter.cs
@@ -0,0 +1,19 @@
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace DevExpress.Avalonia.Theme.Converters;
+
+public class BooleanToChoiceConverter : IMultiValueConverter
+{
+  public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+  {
+    var selectedChoice = values[0] is true ? values[1] : values[2];
+    return selectedChoice ?? AvaloniaProperty.UnsetValue;
+  }
+
+  public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    throw new NotImplementedException();
+  }
+}
