df60c1bf0aa0ac5266547473da37da1b91240bb3
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-04-16 00:50:14 -0400

    [DevExpress] Fix ScrollView for multiline TextBox (#136)

---
+++ src/Devolutions.AvaloniaTheme.DevExpress/Converters/ThicknessToSelectiveThicknessConverter.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace Devolutions.AvaloniaTheme.DevExpress.Converters;
+
+public class ThicknessToSelectiveThicknessConverter : IValueConverter
+{
+  public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    if (value is not Thickness thickness || parameter is not Thickness thicknessFactors)
+      return AvaloniaProperty.UnsetValue;
+
+    return new Thickness(
+      thickness.Left * thicknessFactors.Left,
+      thickness.Top * thicknessFactors.Top,
+      thickness.Right * thicknessFactors.Right,
+      thickness.Bottom * thicknessFactors.Bottom
+    );
+  }
+
+  public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    throw new NotImplementedException();
+  }
+}
