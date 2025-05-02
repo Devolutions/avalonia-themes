ab32448c6b0f345be803744f8c4e9d60ec977c81
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-05-02 16:01:46 -0400

    Add dark mode colour for Combo and NumericUpDown

--- src/Devolutions.AvaloniaTheme.DevExpress/Converters/BooleanAndThemeToChoiceConverter.cs
+++ src/Devolutions.AvaloniaTheme.DevExpress/Converters/BooleanAndThemeToChoiceConverter.cs
@@ -1,14 +1,20 @@
 using System.Globalization;
 using Avalonia;
 using Avalonia.Data.Converters;
+using Avalonia.Styling;
 
 namespace Devolutions.AvaloniaTheme.DevExpress.Converters;
 
-public class BooleanToChoiceConverter : IMultiValueConverter
+public class BooleanAndThemeToChoiceConverter : IMultiValueConverter
 {
   public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
   {
-    object? selectedChoice = values[0] is true ? values[1] : values[2];
+    object? selectedChoice;
+    if (Application.Current?.ActualThemeVariant == ThemeVariant.Dark)
+      selectedChoice = values[0] is true ? values[2] : values[4];
+    else
+      selectedChoice = values[0] is true ? values[1] : values[3];
+
     return selectedChoice ?? AvaloniaProperty.UnsetValue;
   }
 
