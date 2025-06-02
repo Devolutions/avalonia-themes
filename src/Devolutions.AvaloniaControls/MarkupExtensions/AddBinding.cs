76743ea13fc422d02b4818ffa9e800ac93b379cc
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2025-06-02 10:21:30 -0400

    Sadly, variadic constructors (variable parameter numbers with the `params` keyword) don't seem well-supported in axaml. We'll work-around this by exposing 9 constructors with flat parameter numbers, and these markup extensions can be embedded inside one another, so this will cover all possible use-cases, but is less convenient than if variadic were properly supported :(

--- src/Devolutions.AvaloniaControls/MarkupExtensions/AddBinding.cs
+++ src/Devolutions.AvaloniaControls/MarkupExtensions/AddBinding.cs
@@ -5,9 +5,16 @@ using Converters;
 
 public class AddBinding : AbstractMultipleValueBinding<double>
 {
-  public AddBinding(object a, object b) : base(a, b) { }
+    // @formatter:off
+    public AddBinding(object b1, object b2) : base(b1, b2) { }
+    public AddBinding(object b1, object b2, object b3) : base(b1, b2, b3) { }
+    public AddBinding(object b1, object b2, object b3, object b4) : base(b1, b2, b3, b4) { }
+    public AddBinding(object b1, object b2, object b3, object b4, object b5) : base(b1, b2, b3, b4, b5) { }
+    public AddBinding(object b1, object b2, object b3, object b4, object b5, object b6) : base(b1, b2, b3, b4, b5, b6) { }
+    public AddBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7) : base(b1, b2, b3, b4, b5, b6, b7) { }
+    public AddBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7, object b8) : base(b1, b2, b3, b4, b5, b6, b7, b8) { }
+    public AddBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7, object b8, object b9) : base(b1, b2, b3, b4, b5, b6, b7, b8, b9) { }
+    // @formatter:on
 
-  public AddBinding(object a, object b, params object[] extraBindings) : base(a, b, extraBindings) { }
-
-  protected override IMultiValueConverter MultiValueConverter => DevoDoubleConverters.Add;
+    protected override IMultiValueConverter MultiValueConverter => DevoDoubleConverters.Add;
 }
--- src/Devolutions.AvaloniaControls/MarkupExtensions/AndBinding.cs
+++ src/Devolutions.AvaloniaControls/MarkupExtensions/AndBinding.cs
@@ -4,9 +4,16 @@ using Avalonia.Data.Converters;
 
 public class AndBinding : AbstractMultipleValueBinding<bool>
 {
-  public AndBinding(object a, object b) : base(a, b) { }
+    // @formatter:off
+    public AndBinding(object b1, object b2) : base(b1, b2) { }
+    public AndBinding(object b1, object b2, object b3) : base(b1, b2, b3) { }
+    public AndBinding(object b1, object b2, object b3, object b4) : base(b1, b2, b3, b4) { }
+    public AndBinding(object b1, object b2, object b3, object b4, object b5) : base(b1, b2, b3, b4, b5) { }
+    public AndBinding(object b1, object b2, object b3, object b4, object b5, object b6) : base(b1, b2, b3, b4, b5, b6) { }
+    public AndBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7) : base(b1, b2, b3, b4, b5, b6, b7) { }
+    public AndBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7, object b8) : base(b1, b2, b3, b4, b5, b6, b7, b8) { }
+    public AndBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7, object b8, object b9) : base(b1, b2, b3, b4, b5, b6, b7, b8, b9) { }
+    // @formatter:on
 
-  public AndBinding(object a, object b, params object[] extraBindings) : base(a, b, extraBindings) { }
-
-  protected override IMultiValueConverter MultiValueConverter => BoolConverters.And;
+    protected override IMultiValueConverter MultiValueConverter => BoolConverters.And;
 }
