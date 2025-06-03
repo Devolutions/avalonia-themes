864927eb58e98093f143c2350c3d6ba6f2b34bec
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2025-06-03 12:35:36 -0400

    Fix a globalization issue in some of our Markup Extensions (#162)

--- src/Devolutions.AvaloniaControls/Helpers/MarkupExtensionHelpers.cs
+++ src/Devolutions.AvaloniaControls/Helpers/MarkupExtensionHelpers.cs
@@ -1,67 +1,71 @@
 namespace Devolutions.AvaloniaControls.Helpers;
 
 using System.ComponentModel;
+using System.Globalization;
 using Avalonia;
 using Avalonia.Data;
 
 public static class MarkupExtensionHelpers
 {
-  // Dynamic approach is slightly faster than using reflection, see: https://stackoverflow.com/a/78582306
-  public static IBinding? GetBinding<TIn>(object? v)
-  {
-    switch (v)
+    // Dynamic approach is slightly faster than using reflection, see: https://stackoverflow.com/a/78582306
+    public static IBinding? GetBinding<TIn>(object? v)
     {
-      case null:
-        return null;
-      case IBinding binding:
-        return binding;
-      case string str:
-      {
-        var isParsable = typeof(TIn).GetInterfaces()
-          .Any(static c => c.IsGenericType && c.GetGenericTypeDefinition() == typeof(IParsable<>));
-        if (isParsable)
+        switch (v)
         {
-          TIn value = Parse(str, (dynamic)default(TIn)!);
-          return SingleValueObservable(value).ToBinding();
+            case null:
+                return null;
+            case IBinding binding:
+                return binding;
+            case string str:
+            {
+                var isParsable = typeof(TIn).GetInterfaces()
+                    .Any(static c => c.IsGenericType && c.GetGenericTypeDefinition() == typeof(IParsable<>));
+                if (isParsable)
+                {
+                    TIn value = Parse(str, (dynamic)default(TIn)!);
+                    return SingleValueObservable(value).ToBinding();
+                }
+
+                break;
+            }
         }
 
-        break;
-      }
+        if (v is TIn t) return SingleValueObservable(t).ToBinding();
+
+        return TypeDescriptor.GetConverter(v.GetType()).ConvertTo(v, typeof(TIn)) is TIn t2
+            ? SingleValueObservable(t2).ToBinding()
+            : null;
     }
 
-    if (typeof(TIn) == typeof(object)) return SingleValueObservable(v).ToBinding();
-
-    return typeof(TIn) == typeof(object)
-      ? SingleValueObservable(v).ToBinding()
-      : SingleValueObservable(TypeDescriptor.GetConverter(v.GetType()).ConvertTo(v, typeof(TIn))).ToBinding();
-  }
-
-  public static IBinding? GetBinding(object? v, Type type)
-  {
-    switch (v)
+    public static IBinding? GetBinding(object? v, Type type)
     {
-      case null:
-        return null;
-      case IBinding binding:
-        return binding;
-      case string str:
-      {
-        var isParsable = type.GetInterfaces().Any(static c => c.IsGenericType && c.GetGenericTypeDefinition() == typeof(IParsable<>));
-        if (isParsable)
+        switch (v)
         {
-          var parseMethod = type.GetMethod("Parse", [typeof(string)]);
-          return SingleValueObservable(parseMethod?.Invoke(type, [str])).ToBinding();
+            case null:
+                return null;
+            case IBinding binding:
+                return binding;
+            case string str:
+            {
+                var isParsable = type.GetInterfaces().Any(static c => c.IsGenericType && c.GetGenericTypeDefinition() == typeof(IParsable<>));
+                if (isParsable)
+                {
+                    var parseMethod = type.GetMethod("Parse", [typeof(string), typeof(IFormatProvider)]);
+                    return SingleValueObservable(parseMethod?.Invoke(type, [str, CultureInfo.InvariantCulture])).ToBinding();
+                }
+
+                break;
+            }
         }
 
-        break;
-      }
+        var t = TypeDescriptor.GetConverter(v.GetType()).ConvertTo(v, type);
+        return t is not null
+            ? SingleValueObservable(t).ToBinding()
+            : null;
     }
 
-    return SingleValueObservable(TypeDescriptor.GetConverter(v.GetType()).ConvertTo(v, type)).ToBinding();
-  }
+    private static T Parse<T>(string stringValue, T _) where T : IParsable<T> =>
+        T.Parse(stringValue, CultureInfo.InvariantCulture);
 
-  private static T Parse<T>(string stringValue, T _) where T : IParsable<T> =>
-    T.Parse(stringValue, null);
-
-  private static SingleValueObservable<T> SingleValueObservable<T>(T v) => new(v);
+    private static SingleValueObservable<T> SingleValueObservable<T>(T v) => new(v);
 }
