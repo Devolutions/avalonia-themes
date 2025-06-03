059094594d9b7818a54e62e0e01f912fdad2e9fd
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2025-06-03 12:35:59 -0400

    Workaround for Markup Extensions with variable amount of parameters (#163)
    Sadly, variadic constructors (variable parameter numbers with the
    `params` keyword) don't seem well-supported in axaml.
    
    We'll work-around this by exposing 9 constructors with flat parameter
    numbers, and these markup extensions can be embedded inside one another,
    so this will cover all possible use-cases, but is less convenient than
    if variadic were properly supported :(

--- src/Devolutions.AvaloniaControls/MarkupExtensions/OrBinding.cs
+++ src/Devolutions.AvaloniaControls/MarkupExtensions/OrBinding.cs
@@ -4,9 +4,16 @@ using Avalonia.Data.Converters;
 
 public class OrBinding : AbstractMultipleValueBinding<bool>
 {
-  public OrBinding(object a, object b) : base(a, b) { }
+    // @formatter:off
+    public OrBinding(object b1, object b2) : base(b1, b2) { }
+    public OrBinding(object b1, object b2, object b3) : base(b1, b2, b3) { }
+    public OrBinding(object b1, object b2, object b3, object b4) : base(b1, b2, b3, b4) { }
+    public OrBinding(object b1, object b2, object b3, object b4, object b5) : base(b1, b2, b3, b4, b5) { }
+    public OrBinding(object b1, object b2, object b3, object b4, object b5, object b6) : base(b1, b2, b3, b4, b5, b6) { }
+    public OrBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7) : base(b1, b2, b3, b4, b5, b6, b7) { }
+    public OrBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7, object b8) : base(b1, b2, b3, b4, b5, b6, b7, b8) { }
+    public OrBinding(object b1, object b2, object b3, object b4, object b5, object b6, object b7, object b8, object b9) : base(b1, b2, b3, b4, b5, b6, b7, b8, b9) { }
+    // @formatter:on
 
-  public OrBinding(object a, object b, params object[] extraBindings) : base(a, b, extraBindings) { }
-
-  protected override IMultiValueConverter MultiValueConverter => BoolConverters.Or;
+    protected override IMultiValueConverter MultiValueConverter => BoolConverters.Or;
 }
