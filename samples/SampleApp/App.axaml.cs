78ea34d6f0f3ecd1d6e14f5034fa1e33f67d5921
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2025-06-12 10:30:14 -0400

    undo accidental formatting change

--- samples/SampleApp/App.axaml.cs
+++ samples/SampleApp/App.axaml.cs
@@ -171,21 +171,9 @@ public abstract class Theme
 
     public override bool Equals(object? obj)
     {
-        if (obj is null)
-        {
-            return false;
-        }
-
-        if (ReferenceEquals(this, obj))
-        {
-            return true;
-        }
-
-        if (obj.GetType() != this.GetType())
-        {
-            return false;
-        }
-
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
         return this.Equals((Theme)obj);
     }
 
