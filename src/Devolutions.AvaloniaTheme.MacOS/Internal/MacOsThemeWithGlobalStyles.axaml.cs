e10c9cff3fa4e76558e993ab9bb636dab3f6792f
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2025-06-13 13:53:29 -0400

    remove internal comment

--- src/Devolutions.AvaloniaTheme.MacOS/Internal/MacOsThemeWithGlobalStyles.axaml.cs
+++ src/Devolutions.AvaloniaTheme.MacOS/Internal/MacOsThemeWithGlobalStyles.axaml.cs
@@ -3,15 +3,8 @@ namespace Devolutions.AvaloniaTheme.MacOS.Internal;
 using Avalonia.Markup.Xaml;
 using Avalonia.Styling;
 
-/// <summary>
-/// Includes the fluent theme in an application.
-/// </summary>
 internal class MacOsThemeWithGlobalStyles : Styles
 {
-    /// <summary> 
-    /// Initializes a new instance of the <see cref="sp"/> class.
-    /// </summary>
-    /// <param name="sp">The parent's service provider.</param>
     public MacOsThemeWithGlobalStyles(IServiceProvider? sp = null)
     {
         AvaloniaXamlLoader.Load(sp, this);
