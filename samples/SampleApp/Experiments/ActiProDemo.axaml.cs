404e433b9a798bd07d960dd8b1dcc3e93cd45ee1
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-06-19 10:28:36 -0400

    Use ActiPro ToggleThemeButton to re-organize our theme & variant switching UI  (#179)

---
+++ samples/SampleApp/Experiments/ActiProDemo.axaml.cs
@@ -0,0 +1,17 @@
+namespace SampleApp.Experiments;
+
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+public partial class ActiProDemo : UserControl
+{
+  public ActiProDemo()
+  {
+    this.InitializeComponent();
+  }
+
+  private void OnHyperlinkTextBlockClicked(object? sender, RoutedEventArgs e)
+  {
+    // Handle click here
+  }
+}
