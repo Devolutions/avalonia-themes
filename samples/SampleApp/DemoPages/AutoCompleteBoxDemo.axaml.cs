fbb80c8e741f11bbfc8d3861ea09b8c51b24f0a7
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-07-10 12:23:33 -0400

    Documentation

--- samples/SampleApp/DemoPages/AutoCompleteBoxDemo.axaml.cs
+++ samples/SampleApp/DemoPages/AutoCompleteBoxDemo.axaml.cs
@@ -1,18 +1,15 @@
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
-
 namespace SampleApp.DemoPages;
 
 using System.Linq;
+using Avalonia.Controls;
 
 public partial class AutoCompleteBoxDemo : UserControl
 {
   public AutoCompleteBoxDemo()
   {
     InitializeComponent();
-    this.Animals.ItemsSource = new string[] 
-        {"cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra" }
+    this.Animals.ItemsSource = new string[]
+        { "cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra", "dog", "frog", "jackal", "puffin", "quokka", "vole", "oryx" }
       .OrderBy(x=>x);
   }
 }
