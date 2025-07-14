c15fdb9f03dcf98ddc432d5f7ff3fb53ba29fae3
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2025-07-14 16:19:46 -0400

    Ensure TabItem's theme is user-editable in TabPane

--- src/Devolutions.AvaloniaControls/Controls/TabPane.cs
+++ src/Devolutions.AvaloniaControls/Controls/TabPane.cs
@@ -1,17 +1,28 @@
 namespace Devolutions.AvaloniaControls.Controls;
 
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Styling;
 
 public class TabPane : TabControl
 {
+    public static readonly StyledProperty<ControlTheme?> TabItemThemeProperty =
+        AvaloniaProperty.Register<EditableComboBoxItem, ControlTheme?>(nameof(TabItemTheme));
+
+    public ControlTheme? TabItemTheme
+    {
+        get => this.GetValue(TabItemThemeProperty);
+        set => this.SetValue(TabItemThemeProperty, value);
+    }
+
     protected override void PrepareContainerForItemOverride(Control element, object? item, int index)
     {
         base.PrepareContainerForItemOverride(element, item, index);
 
-        if (element.FindResource("TabPane_TabItem") is ControlTheme theme)
+        ControlTheme? tabItemTheme = this.TabItemTheme ?? element.FindResource("TabPane_TabItem") as ControlTheme;
+        if (tabItemTheme is not null)
         {
-            element.Theme = theme;
+            element.Theme = tabItemTheme;
         }
     }
 }
