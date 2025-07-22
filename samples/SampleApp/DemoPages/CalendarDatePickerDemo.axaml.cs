66f3ff4c97b759367e90e63d0596599a6b5db9e8
Anna Malchow-Perryman <amalchowperryman@devolutions.net>
2025-07-22 19:32:42 -0400

    [MacOS] Fix DatePicker width for long format & remove format presets (#210)

--- samples/SampleApp/DemoPages/CalendarDatePickerDemo.axaml.cs
+++ samples/SampleApp/DemoPages/CalendarDatePickerDemo.axaml.cs
@@ -8,6 +8,6 @@ public partial class CalendarDatePickerDemo : UserControl
   public CalendarDatePickerDemo()
   {
     this.InitializeComponent();
-    this.DatePicker.SelectedDate = this.DatePicker2.SelectedDate = DateTime.Today;
+    this.DatePicker.SelectedDate = this.DatePicker2.SelectedDate = this.DatePicker3.SelectedDate = DateTime.Today;
   }
 }
